22c2e8adeb2a2e50165f70a2726e26ad6cbb64d0
Andrea Baldelomar <anbade@gmail.com>
2015-10-30 14:27:38 -0400

    Change styles to nav bar and pop up menu

--- app/src/main/java/com/android/diplomado/postjob/PostJob.java
+++ app/src/main/java/com/android/diplomado/postjob/PostJob.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
